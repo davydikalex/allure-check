e45216a8694d57b054b9efb0f9f326f13076658f
Давыдик Алексей Николаевич <andavy10@mts.ru>
2025-04-04 10:41:00 +0300

    first3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'allure_check = allure_check:main',
+            'allure_check = allure_check.allure_check:main',
         ],
     },
 )
